750032c842f2cfd69383a5cb5599ba4651690b5a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-06-10 20:02:46 +0600

    fix

--- seeder_flet/main.py
+++ seeder_flet/main.py
@@ -1,5 +1,6 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -7,12 +8,14 @@ def main(page: ft.Page):
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
     def minus_click(e):
-        if not txt_number.value: return
+        if not txt_number.value:
+            return
         txt_number.value = str(int(txt_number.value) - 1)
         page.update()
 
     def plus_click(e):
-        if not txt_number.value: return
+        if not txt_number.value:
+            return
         txt_number.value = str(int(txt_number.value) + 1)
         page.update()
 
@@ -27,4 +30,6 @@ def main(page: ft.Page):
         )
     )
 
-ft.app(main)
+
+if __name__ == "__main__":
+    ft.app(main)
