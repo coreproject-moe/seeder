2bb247c116b19cf032dc7d72c0ad2257c10384c4
tokitouq <suneeth.since2005@gmail.com>
2024-06-12 22:16:23 +0530

    feat: routing

--- src/seeder_flet/main.py
+++ src/seeder_flet/main.py
@@ -16,11 +16,42 @@ def main(page: ft.Page):
         )
     )
 
-    page.add(ft.Text("coreproject", color=ft.colors.BLUE_100))
-    page.bgcolor = ft.colors.SECONDARY
+    def route_change(event: ft.RouteChangeEvent):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.Text("Login page", color=ft.colors.BLUE_100),
+                    ft.TextButton("Go to anime", on_click=lambda _: page.go("/home"))
+                ],
+                bgcolor=ft.colors.SECONDARY,
+                padding=0
+            )
+        )
+        if event.route == "/home":
+            page.views.append(
+                ft.View(
+                    "/",
+                    [
+                        ft.Text("Home", color=ft.colors.BLUE_100),
+                        ft.TextButton("Login", on_click=lambda _: page.go("/"))
+                    ],
+                    bgcolor=ft.colors.SECONDARY,
+                    padding=0
+                )
+            )
 
-    page.update()
+        page.update()
 
+    def view_pop(event: ft.ViewPopEvent):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
 
 if __name__ == "__main__":
     ft.app(main, assets_dir="assets/")
