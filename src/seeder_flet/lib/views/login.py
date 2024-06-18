df2fe26c409aaaf6ade15cc0b8b44d98b4758aa5
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-06-18 18:40:41 +0600

    make everything async

--- src/seeder_flet/lib/views/login.py
+++ src/seeder_flet/lib/views/login.py
@@ -3,49 +3,54 @@ import flet as ft
 __all__ = ["login_view"]
 
 
-def handle_login_click():
-    pass
+async def handle_login_click(username:str, password:str, backend:str):
+    "Try to login with backend"
+    ...
 
 
-def login_view(page: ft.Page):
+async def login_view():
+    username_or_email_field = ft.TextField(
+        label="Username/Email address",
+        width=400,
+        height=50,
+        # hint_text="soraamamiya#4444",
+        border_color=ft.colors.TERTIARY,
+        focused_border_color=ft.colors.PRIMARY,
+        border_radius=10,
+        label_style=ft.TextStyle(color=ft.colors.BLUE_100),
+    )
+    password_field = ft.TextField(
+        label="Password",
+        password=True,
+        can_reveal_password=True,
+        # hint_text="soraamamiya",
+        width=400,
+        height=50,
+        border_color=ft.colors.TERTIARY,
+        focused_border_color=ft.colors.PRIMARY,
+        border_radius=10,
+        label_style=ft.TextStyle(color=ft.colors.BLUE_100),
+    )
+    backend_url_field = ft.TextField(
+        label="Backend URL",
+        hint_text="backend.coreproject.moe",
+        width=400,
+        height=50,
+        border_color=ft.colors.TERTIARY,
+        focused_border_color=ft.colors.PRIMARY,
+        border_radius=10,
+        label_style=ft.TextStyle(color=ft.colors.BLUE_100),
+    )
+
     return ft.Row(
         expand=True,
         controls=[
             ft.Column(
                 [
                     ft.Text("Login to Continue", color=ft.colors.BLUE_100, size=20),
-                    ft.TextField(
-                        label="Username/Email address",
-                        width=400,
-                        height=50,
-                        # hint_text="soraamamiya#4444",
-                        border_color=ft.colors.TERTIARY,
-                        focused_border_color=ft.colors.PRIMARY,
-                        border_radius=10,
-                        label_style=ft.TextStyle(color=ft.colors.BLUE_100),
-                    ),
-                    ft.TextField(
-                        label="Password",
-                        password=True,
-                        can_reveal_password=True,
-                        # hint_text="soraamamiya",
-                        width=400,
-                        height=50,
-                        border_color=ft.colors.TERTIARY,
-                        focused_border_color=ft.colors.PRIMARY,
-                        border_radius=10,
-                        label_style=ft.TextStyle(color=ft.colors.BLUE_100),
-                    ),
-                    ft.TextField(
-                        label="Backend URL",
-                        hint_text="backend.coreproject.moe",
-                        width=400,
-                        height=50,
-                        border_color=ft.colors.TERTIARY,
-                        focused_border_color=ft.colors.PRIMARY,
-                        border_radius=10,
-                        label_style=ft.TextStyle(color=ft.colors.BLUE_100),
-                    ),
+                    username_or_email_field,
+                    password_field,
+                    backend_url_field,
                     ft.FilledButton(
                         "Log in",
                         style=ft.ButtonStyle(
@@ -53,7 +58,7 @@ def login_view(page: ft.Page):
                             color={ft.MaterialState.DEFAULT: ft.colors.BLUE_100},
                             bgcolor=ft.colors.TERTIARY,
                         ),
-                        on_click=handle_login_click(),
+                        on_click=handle_login_click(username_or_email_field.value,password_field.value,backend_url_field.value),
                         height=40,
                         width=200,
                     ),
